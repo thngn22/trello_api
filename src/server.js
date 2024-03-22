cdbb3ed8e3d1f26a9e460bc2e6e12767755e3d85
thngn22 <thangnguyen69962002@gmail.com>
2024-03-22 21:48:14 +0700

    setup routes

--- src/server.js
+++ src/server.js
@@ -1,28 +1,15 @@
-/* eslint-disable no-console */
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
-
 import express from 'express'
 import exithook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
+import { APIs_V1 } from './routes/v1'
 
 const START_SERVER = () => {
   const app = express()
 
-  const hostname = 'localhost'
-  const port = 8017
+  app.use('/v1', APIs_V1)
 
-  app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-
-    res.end('<h1>Hello World!</h1><hr>')
-  })
-
-  app.listen(port, hostname, () => {
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
   // eslint-disable-next-line no-console
     console.log(`Hello ${ env.AUTHOR }, I am running at http://${ env.APP_HOST }:${ env.APP_PORT }/`)
   })
