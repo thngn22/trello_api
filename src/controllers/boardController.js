d646b32b15345d2de940741c547a8ea7bc280903
thngn22 <thangnguyen69962002@gmail.com>
2024-03-24 11:49:32 +0700

    add one bottom line into ApiError.js

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,13 +1,13 @@
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
-const createNew = (req, res) => {
+const createNew = (req, res, next) => {
   try {
     console.log('req.body in Controller', req.body)
+    // throw new ApiError(StatusCodes.BAD_GATEWAY, 'cc gi do')
     res.status(StatusCodes.CREATED).json({ message: 'POST from Controller: API create new board', code: `${ StatusCodes.CREATED }` })
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
+    next(error)
   }
 }
 
---
+++ src/middlewares/errorHandlingMiddleware.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import { StatusCodes } from 'http-status-codes'
+// import { env } from '~/config/environment'
+
+export const errorHandlingMiddleware = (err, req, res, next) => {
+  if (!err.statusCode) err.statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+
+  const responseError = {
+    statusCode: err.statusCode,
+    message: err.message || StatusCodes[err.statusCode],
+    stack: err.stack
+  }
+  // console.error(responseError)
+
+  // Chỉ khi môi trường là DEV thì mới trả về Stack Trace để debug dễ dàng hơn, còn không thì xóa đi. (Muốn hiểu rõ hơn hãy xem video 55 trong bộ MERN Stack trên kênh Youtube: https://www.youtube.com/@trungquandev)
+  // if (env.BUILD_MODE !== 'dev') delete responseError.stack
+
+  // Đoạn này có thể mở rộng nhiều về sau như ghi Error Log vào file, bắn thông báo lỗi vào group Slack, Telegram, Email...vv Hoặc có thể viết riêng Code ra một file Middleware khác tùy dự án.
+  // ...
+  // console.error(responseError)
+
+  // Trả responseError về phía Front-end
+  res.status(responseError.statusCode).json(responseError)
+}
